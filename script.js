d572169fe8c01cd66137fadb6cf047547f7c0efc
juanjoffre-wq <juan.joffre@uab.edu.bo>
2026-08-04 21:44:19 -0400

    Js aplicadook

--- script.js
+++ script.js
@@ -14,3 +14,33 @@ function cambiarModo() {
 }
 
 botonModo.addEventListener("click", cambiarModo);
+
+
+const formulariPedido = document.querySelector("#form-pedido");
+const avisoPedido = document.querySelector("#error-pedido");
+
+
+
+function revisarPedido(event) {
+    event.preventDefault();
+    const nombre = document.querySelector("#nombre").value;
+    const correo = document.querySelector("#correo").value;   
+
+
+if (nombre === "") {
+    avisoPedido.textContent = "Falta tu nombre, caserito.";
+    avisoPedido.classList.add("error");
+    avisoPedido.classList.remove("exito");
+} else if (correo.includes("@") === false) {
+    avisoPedido.textContent = "Ese correo no parece correo: le falta el @.";
+    avisoPedido.classList.add("error");
+    avisoPedido.classList.remove("exito");
+} else {
+    avisoPedido.textContent = "Pedido recibido, caserito. Te contactamos hoy.";
+    avisoPedido.classList.add("exito");
+    avisoPedido.classList.remove("error");
+}
+
+}
+
+formulariPedido.addEventListener("submit", revisarPedido);
