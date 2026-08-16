8bc7f1a909bfa14159fbd3d10f1637dd29a845a3
juanjoffre-wq <juan.joffre@uab.edu.bo>
2026-08-16 18:29:28 -0400

    practica php

---
+++ php-practica/procesar.php
@@ -0,0 +1,9 @@
+<?php
+$nombre = $_POST["nombre"];
+$correo = $_POST["correo"];
+$mensaje = $_POST["mensaje"];
+echo "<h2>mensaje recibido, caserito</h2>";
+echo "<p>Nombre: $nombre</p>";
+echo "<p>Correo: $correo</p>";
+echo "<p>mensaje: $mensaje</p>";
+?>
